c432eba38288b89ba975cc410f1692c3a804f510
Thomas Dupoiron <tdupoiron@github.com>
2024-01-19 16:06:41 +0000

    Add isPrime function to check if a number is prime

--- app.js
+++ app.js
@@ -3,18 +3,24 @@
 function primeNumbers(n) {
   var primes = []; // Create an empty array to store prime numbers
   for (var i = 2; i <= n; i++) {
-    var isPrime = true;
-    for (var j = 2; j <= Math.sqrt(i); j++) {
-      if (i % j === 0) {
-        isPrime = false;
-        break;
-      }
-    }
-    if (isPrime) {
+    if (isPrime(i)) {
       primes.push(i); // Add prime number to the array
     }
   }
   console.log(primes); // Display the full array of prime numbers
+}
+
+// generate a function to test is a number is prime
+
+function isPrime(n) {
+  var isPrime = true;
+  for (var j = 2; j <= Math.sqrt(n); j++) {
+    if (n % j === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  return isPrime;
 }
 
 // main
