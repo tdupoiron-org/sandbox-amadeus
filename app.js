65f9facaa9ce84ad46696a81e6edf789ab4687a7
Thomas Dupoiron <tdupoiron@github.com>
2024-01-19 15:35:17 +0000

    Refactor primeNumbers function to improve performance

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 // create a function to list prime numbers from 0 to 100
 
 function primeNumbers() {
-  for (var i = 0; i <= 100; i++) {
+  for (var i = 2; i <= 100; i++) {
     var isPrime = true;
-    for (var j = 2; j <= i; j++) {
-      if (i % j === 0 && j !== i) {
+    for (var j = 2; j <= Math.sqrt(i); j++) {
+      if (i % j === 0) {
         isPrime = false;
+        break;
       }
     }
-    if (isPrime === true) {
+    if (isPrime) {
       console.log(i);
     }
   }
